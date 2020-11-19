4d73b07386b8fcbef73985659039d6834dddcfeb
RozeGift <72850017+RozeGift@users.noreply.github.com>
2020-11-19 22:09:13 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject CoinCollected;
+    public float speed = 10.0f;
+    private int coinCount;
+    private int totalCoin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+                                                                            
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        /*
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            
+        }
+        */
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0,90, 0);
+        }
+        if(coinCount == totalCoin)
+        {
+            //print("You Win!");
+            SceneManager.LoadScene("WinScene");
+        }
+        if(transform.position.y < -3)
+        {
+            //print("You Lose");
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin") 
+        {
+            coinCount++;
+
+            CoinCollected.GetComponent<Text>().text = "Coins Collected:" + coinCount;
+            Destroy(other.gameObject);
+
+        }
+    }
+}
